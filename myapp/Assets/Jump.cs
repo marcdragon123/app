dc08dbb6b5cb865efa5cbade2632900de7e0efb3
marcdragon123 <thedragon_master@hotmail.ca>
2015-10-10 20:17:12 -0400

    Hello

---
+++ myapp/Assets/Jump.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.Space))
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(Vector3.up, ForceMode2D.Impulse);
+        }
+	}
+}
---
+++ myapp/Assets/movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+    public float timeConstant=2.0f;
+
+    void Start() {
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Vector3 position = this.transform.position;
+            //position.x++;
+            //this.transform.position = position;
+            this.transform.Translate(Vector3.right*timeConstant*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Vector3 position = this.transform.position;
+            //position.x--;
+            //this.transform.position = position;
+            this.transform.Translate(Vector3.left * timeConstant * Time.deltaTime);
+        }
+    }
+}
